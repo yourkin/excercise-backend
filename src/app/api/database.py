37e39e04fec83f0e946db019a08399c87f34e0bc
Andrew Yourkin <andrew@yourkin.io>
2023-08-03 12:48:06 +0200

    Updated isort version to fix incompatibility & reformatted

--- src/app/api/database.py
+++ src/app/api/database.py
@@ -1,13 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import Session, sessionmaker
 from sqlmodel import SQLModel, create_engine
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from src.app.api.models import OrderDB, CreateOrderModel
-from src.app.celery_app import celery_app
 
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/postgres"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
 
 async def get_session() -> Session:
     async with async_session() as session:
