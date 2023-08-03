9693d4fe560fbccb1a024c0f10f139258d1b44b1
Andrew Yourkin <andrew@yourkin.io>
2023-08-03 12:26:19 +0200

    Updated main for startup and shutdown events

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,28 @@
+import logging
+
 from fastapi import FastAPI
 
 from src.app.api.v1.router import router as v1_router
 from src.app.api.database import create_tables
 
+log = logging.getLogger("uvicorn")
+
+
 def create_application() -> FastAPI:
     app = FastAPI()
-
-    @app.on_event("startup")
-    async def startup_event():
-        create_tables()
-
     app.include_router(v1_router, prefix="/v1")
     return app
 
+
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    create_tables()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
