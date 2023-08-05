8edf835f33011cff1c59f8b2ab52a156fd9dbdc1
Andrew Yourkin <andrew@yourkin.io>
2023-08-05 19:16:59 +0200

    Fixed typo in run tests"

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@
 source .env
 
 if [[ "${ENVIRONMENT}" == "production" ]]; then
-  docker-compose -f -f docker-compose-prod.yml run web pytest -vs
+  docker-compose -f docker-compose-prod.yml run web pytest -vs
 else
   docker-compose -f docker-compose-dev.yml run web pytest -vs ../tests
 fi
