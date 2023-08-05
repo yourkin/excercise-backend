0df7f0c921945d6960e29ec1312df3e9774dc290
Andrew Yourkin <andrew@yourkin.io>
2023-08-05 18:39:16 +0200

    Fixed create order test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,34 @@
+from datetime import datetime
+
+import dateutil.parser
+
+
 def test_create_order(client):
-    response = client.post(
-        "/v1/orders",
-        json={
-            "type": "limit",
-            "side": "buy",
-            "instrument": "BTCUSD",
-            "limit_price": 50000.0,
-            "quantity": 1,
-        },
-    )
-    assert response.status_code == 200, response.text
-    data = response.json()
-    assert data["type"] == "limit"
-    assert data["side"] == "buy"
-    assert data["instrument"] == "BTCUSD"
-    assert data["limit_price"] == 50000.0
-    assert data["quantity"] == 1
-    assert "id" in data
-    assert "created_at" in data
+    # Test data
+    order_data = {
+        "type": "limit",
+        "side": "buy",
+        "instrument": "abcdefghijkl",
+        "limit_price": 150.00,
+        "quantity": 10,
+    }
+    # Make a request to the server
+    response = client.post("/v1/orders/", json=order_data)
+
+    # Assert that the response status code is 201 (Created)
+    assert response.status_code == 201
+
+    # Assert that the response data matches the test data
+    response_data = response.json()
+    assert response_data["type"] == order_data["type"]
+    assert response_data["side"] == order_data["side"]
+    assert response_data["instrument"] == order_data["instrument"]
+    assert response_data["limit_price"] == order_data["limit_price"]
+    assert response_data["quantity"] == order_data["quantity"]
+
+    # Parse string to datetime object
+    created_at = dateutil.parser.parse(response_data["created_at"])
+    assert isinstance(created_at, datetime)
 
 
 def test_create_order_invalid_input(client):
