b972365a8f73209d3fbfd626f088a3507f507e6a
Andrew Yourkin <andrew@yourkin.io>
2023-08-05 19:35:22 +0200

    Added a wait for it script and updated run tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,12 @@
 # Read the environment variables
 source .env
 
+# Start PostgreSQL
+docker-compose -f docker-compose-psql.yml up -d
+
+# Wait for PostgreSQL to start
+./wait-for-it.sh db:5432 --timeout=30
+
 if [[ "${ENVIRONMENT}" == "production" ]]; then
   docker-compose -f docker-compose-prod.yml run web pytest -vs
 else
