e6010b79866b441bfbd6ccb6b1cc4955fbd91b98
Andrew Yourkin <andrew@yourkin.io>
2023-08-22 01:57:15 +0200

    Using python-decouple for configuration in settings

--- src/ex_back/config.py
+++ src/ex_back/config.py
@@ -1,19 +1,19 @@
 import logging
-import os
 from functools import lru_cache
 
+from decouple import config
 from pydantic import BaseSettings, PostgresDsn
 
 log = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    environment: str = os.getenv("ENVIRONMENT", "dev")
-    testing: bool = os.getenv("TESTING", False)
-    database_url: PostgresDsn = os.environ.get("DATABASE_URL")
-    test_database_url: PostgresDsn = os.environ.get("TEST_DATABASE_URL")
-    rabbit_mq_url: str = os.environ.get("RABBIT_MQ_URL")
-    celery_broker_url: str = os.environ.get("CELERY_BROKER_URL")
+    environment: str = config("ENVIRONMENT", default="dev")
+    testing: bool = config("TESTING", default=False, cast=bool)
+    database_url: PostgresDsn = config("DATABASE_URL", cast=str)
+    test_database_url: PostgresDsn = config("TEST_DATABASE_URL", cast=str)
+    rabbit_mq_url: str = config("RABBIT_MQ_URL", default="")
+    celery_broker_url: str = config("CELERY_BROKER_URL", default="")
 
     @property
     def sync_database_url(self) -> str:
